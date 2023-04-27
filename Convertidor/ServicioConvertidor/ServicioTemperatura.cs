9115053c4da1edbd81515296059f2861d6845ed8
JEnriqueF <jesus.enrique.fernandezgonzalez@gmail.com>
2023-04-27 00:53:00 -0600

    C2 - 27/04/2023 - Debería contener la carpeta de la tarea de convertor

---
+++ Convertidor/ServicioConvertidor/ServicioTemperatura.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicioConvertidor {
+    internal class ServicioTemperatura : IServicioTemperatura {
+        public float convertirCelsiusAFahrenheit(float celsius) {
+            float resultadoFahhrenheit = celsius * 1.8f + 32;
+            return resultadoFahhrenheit;
+        }
+
+        public float convertirFahrenheitACelsius(float fahrenheit) {
+            float resultadoCelsius = fahrenheit - 32 / 1.8f;
+            return resultadoCelsius;
+        }
+    }
+}
